4d92e78260016cce9e469e032f0ade64d4ed9301
Venus Poon <venuspsw@hotmail.com>
2026-03-09 10:32:05 +0100

    remove invalid nul file

--- include/button.h
+++ include/button.h
@@ -5,6 +5,9 @@
 
 static volatile uint8_t last_rotor_clk_state = 1;
 static volatile uint8_t rotor_state_idx = 0;
+static volatile 
+
+
 static const ROTOR_STATE rotor_state_list[ROTOR_ST_COUNT] = {
     ROTOR_ST_OFF,
     ROTOR_ST_RED,
@@ -14,13 +17,17 @@ static const ROTOR_STATE rotor_state_list[ROTOR_ST_COUNT] = {
 
 static void rotor_init(void)
 {
-    DDRD &= ~(ROTOR_CLK | ROTOR_DT); // set as input
-    PORTD |= ROTOR_CLK | ROTOR_DT;
+    DDRD &= ~(ROTOR_CLK | ROTOR_DT | ROTOR_SW); // set as input
+    PORTD |= ROTOR_CLK | ROTOR_DT | ROTOR_SW;
 
-    EICRA |= (1 << ISC01); // falling edge
+    EICRA |= (1 << ISC01); // falling edge CLK DT
     EICRA &= ~(1 << ISC00);
 
+    EICRA |= (1 << ISC11);// falling edge SW
+EICRA &= ~(1 << ISC10); 
+
     EIMSK |= (1 << INT0); // enable INT0
+    EIMSK |= (1 << INT1);// enable INT1
 }
 
 ISR(INT0_vect)
@@ -39,4 +46,8 @@ ISR(INT0_vect)
     PORTD |= rotor_state_list[rotor_state_idx];
 }
 
+
+ISR(INT1_vect){
+
+}
 #endif
